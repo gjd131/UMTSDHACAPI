d0700e20d302d4b2c006d6e6256535e1d3b3c772
gjd131 <37119691+gjd131@users.noreply.github.com>
2025-08-26 13:29:17 -0400

    Change to UMTSD

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<Props> = ({ children }) => {
     <>
     <Head>
       <title>Frisco ISD HAC API</title>
-      <meta name="description" content="REST API to access student data from Home Access Center (Frisco ISD) "></meta>
+      <meta name="description" content="REST API to access student data from Home Access Center (UMTSD) "></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
       <div className="bg-background flex flex-col h-full text-paragraph">
