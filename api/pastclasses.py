5793099ce825858a1f5aa608a66a148a6ac0c4d4
gjd131 <37119691+gjd131@users.noreply.github.com>
2025-08-26 12:57:08 -0400

    Update pastclasses.py
    Change to UMTSD

--- api/pastclasses.py
+++ api/pastclasses.py
@@ -18,11 +18,11 @@ class handler(BaseHTTPRequestHandler):
     session = getRequestSession(username, password)
 
     pastCoursesRequestHeaders = {
-        "Host": "hac.mckinneyisd.net",
+        "Host": "esp41pehac.eschoolplus.powerschool.com",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0",
         'X-Requested-With': 'XMLHttpRequest',
-        "Origin": "https://hac.mckinneyisd.net",
-        "Referer": "https://hac.mckinneyisd.net/HomeAccess/Content/Student/Assignments.aspx"
+        "Origin": "https://esp41pehac.eschoolplus.powerschool.com",
+        "Referer": "https://esp41pehac.eschoolplus.powerschool.com/HomeAccess/Content/Student/Assignments.aspx"
     }
 
     pastCoursesRequestPayload = {
